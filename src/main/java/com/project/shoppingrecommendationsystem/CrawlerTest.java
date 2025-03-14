4ed9deabf096e1793090dd1a1f0ddc4d46430f0c
Vũ Long <lnvlong.dev@gmail.com>
2025-03-14 12:02:43 +0700

    ✨: Add FPTShopCrawler to get product basic data and save them to FPTShop.csv

---
+++ src/main/java/com/project/shoppingrecommendationsystem/CrawlerTest.java
@@ -0,0 +1,9 @@
+package com.project.shoppingrecommendationsystem;
+
+import com.project.shoppingrecommendationsystem.models.FPTShopCrawler;
+
+public class CrawlerTest {
+    public static void main(String[] args) {
+        FPTShopCrawler crawler = new FPTShopCrawler();
+    }
+}
---
+++ src/main/java/com/project/shoppingrecommendationsystem/models/Laptop.java
@@ -0,0 +1,8 @@
+package com.project.shoppingrecommendationsystem.models;
+
+public class Laptop extends Product{
+    public Laptop(int id, String name, String description, int price, String sourceURL) {
+        super(id, name, description, price, sourceURL);
+    }
+}
+

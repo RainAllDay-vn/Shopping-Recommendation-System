b9641c8147ca6d762707c991c820fa81ce0a6a74
Hieu1223 <minhhieunn2000@gmail.com>
2025-03-14 11:58:14 +0700

    copy files from old repo

---
+++ src/main/java/com/project/shoppingrecommendationsystem/models/Crawler.java
@@ -0,0 +1,5 @@
+package com.project.shoppingrecommendationsystem.models;
+
+public abstract class Crawler {
+    public abstract void crawl();
+}
---
+++ src/main/java/com/project/shoppingrecommendationsystem/models/Laptop.java
@@ -0,0 +1,7 @@
+package com.project.shoppingrecommendationsystem.models;
+
+public class Laptop extends Product{
+    public Laptop(int id, String name, String description, int price, String sourceURL) {
+        super(id, name, description, price, sourceURL);
+    }
+}

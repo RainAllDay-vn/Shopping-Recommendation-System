19f293f767504c38def3444b3a2894bea28cabf6
Feng20-24 <Phong.PT235987@sis.hust.edu.vn>
2025-03-22 16:12:53 +0700

    ⬇️ fetch database

---
+++ src/main/java/com/project/shoppingrecommendationsystem/models/ProductDatabase.java
@@ -0,0 +1,40 @@
+package com.project.shoppingrecommendationsystem.models;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductDatabase {
+    private final List<Laptop> laptops;
+
+    public ProductDatabase() {
+        laptops = new ArrayList<>();
+    }
+
+    public void loadTestLaptop() {
+        for (int i = 0; i < 10; i++) {
+            laptops.add(Laptop.buildTestLaptop());
+        }
+    }
+
+    public List<Laptop> findAllLaptops() {
+        return laptops;
+    }
+
+    public Optional<Laptop> findLaptopById(int id) {
+        for (Laptop laptop : laptops) {
+            LinkedHashMap<String, String> overview = laptop.getOverview();
+            if (overview.getOrDefault("id", "").equals(String.valueOf(id))) {
+                return Optional.of(laptop);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public List<Laptop> findLaptop (String[][] query) {
+        return laptops.stream()
+                .filter(laptop -> laptop.match(query))
+                .toList();
+    }
+}

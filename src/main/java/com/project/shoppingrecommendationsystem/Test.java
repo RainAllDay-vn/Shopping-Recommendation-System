c7d4f9ad556e4263bcf6f79679fec6421242cf95
Vũ Long <lnvlong.dev@gmail.com>
2025-05-21 09:00:47 +0700

    🔧: Renew pre-crawl product descriptions

--- src/main/java/com/project/shoppingrecommendationsystem/Test.java
+++ src/main/java/com/project/shoppingrecommendationsystem/Test.java
@@ -1,18 +1,13 @@
 package com.project.shoppingrecommendationsystem;
 
-import com.project.shoppingrecommendationsystem.models.Laptop;
+import com.project.shoppingrecommendationsystem.models.Product;
 import com.project.shoppingrecommendationsystem.models.ProductDatabase;
-import com.project.shoppingrecommendationsystem.models.crawler.*;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        Crawler crawler = new CellphoneSLaptopCrawler();
-        crawler.crawl(1);
-        for (String[] descriptions: crawler.getAll().getFirst().getDescription()) {
-            System.out.println(Arrays.toString(descriptions));
-        }
+        ProductDatabase database = ProductDatabase.getInstance();
+        database.crawl();
     }
 }

7279546bd1d1859c8b0b0c800f46d066a078ad54
Feng20-24 <Phong.PT235987@sis.hust.edu.vn>
2025-05-23 11:47:04 +0700

    :recycle: Use Add chat box toggle image

--- src/main/java/com/project/shoppingrecommendationsystem/controllers/OverlayController.java
+++ src/main/java/com/project/shoppingrecommendationsystem/controllers/OverlayController.java
@@ -1,10 +1,28 @@
 package com.project.shoppingrecommendationsystem.controllers;
 
 import com.project.shoppingrecommendationsystem.Messenger;
+import com.project.shoppingrecommendationsystem.ShoppingApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class OverlayController {
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class OverlayController implements Initializable {
     private final MainPageController mainPageController = Messenger.getInstance().getMainPageController();
+    private final Image chatLogo = new Image(Objects.requireNonNull(
+            ShoppingApplication.class.getResource("images/chat-bot-icon.png")).toExternalForm());
+    @FXML
+    private ImageView chatButton;
+
+    @Override
+    public void initialize (URL url, ResourceBundle resourceBundle) {
+        chatButton.setImage(chatLogo);
+        chatButton.setOnMouseClicked(event -> toggleChatBox());
+    }
 
     @FXML
     public void toggleChatBox () {

7bdbc78542d2a1af19adeeaf83e649b39035d5bc
Vũ Long <lnvlong.dev@gmail.com>
2025-05-28 05:55:26 +0700

    Push new data

--- src/main/java/com/project/shoppingrecommendationsystem/Test.java
+++ src/main/java/com/project/shoppingrecommendationsystem/Test.java
@@ -1,9 +1,13 @@
 package com.project.shoppingrecommendationsystem;
 
+import com.project.shoppingrecommendationsystem.models.database.ListDatabase;
+import com.project.shoppingrecommendationsystem.models.database.ProductDatabase;
+
 import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-
+        ProductDatabase productDatabase = new ListDatabase();
+        productDatabase.crawl();
     }
 }

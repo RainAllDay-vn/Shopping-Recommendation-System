538ef81a6dea5dee2779ad50f896d657aa934492
Vũ Long <lnvlong.dev@gmail.com>
2025-05-27 17:27:16 +0700

    ♻️: Fix ProcessEmbedding

--- src/main/java/com/project/shoppingrecommendationsystem/llmagent/ProcessEmbedding.java
+++ src/main/java/com/project/shoppingrecommendationsystem/llmagent/ProcessEmbedding.java
@@ -1,10 +1,11 @@
 package com.project.shoppingrecommendationsystem.llmagent;
 
+import com.project.shoppingrecommendationsystem.Messenger;
 import com.project.shoppingrecommendationsystem.llmagent.embedmodel.EmbedModel;
 import com.project.shoppingrecommendationsystem.llmagent.embedmodel.VertexEmbedModel;
 import com.project.shoppingrecommendationsystem.llmagent.vectordatabase.QdrantVectorDatabase;
-import com.project.shoppingrecommendationsystem.models.Laptop;
-import com.project.shoppingrecommendationsystem.models.database.LaptopDatabase;
+import com.project.shoppingrecommendationsystem.models.Product;
+import com.project.shoppingrecommendationsystem.models.database.ProductDatabase;
 import org.springframework.ai.document.Document;
 
 import java.io.IOException;
@@ -14,25 +15,24 @@ import java.util.stream.Collectors;
 
 public class ProcessEmbedding {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        LaptopDatabase rawDatabase = LaptopDatabase.getInstance();
+        ProductDatabase rawDatabase = Messenger.getInstance().getProductDatabase();
         EmbedModel embedding = new VertexEmbedModel();
         String storeName = "Shopping Recommendation System";
         QdrantVectorDatabase vectorStore = new QdrantVectorDatabase(storeName, embedding);
 
-        List<Laptop> laptopList = rawDatabase.findAllLaptops();
+        List<Product> products = rawDatabase.findAllProducts();
 
-        List<Document> documents = laptopList.stream()
-                .map(laptop -> {
+        List<Document> documents = products.stream()
+                .map(product -> {
                     Map<String, Object> metadata = new HashMap<>();
-                    metadata.put("id", laptop.getId());
-                    metadata.put("name", laptop.getName());
-                    metadata.put("price", laptop.getPrice());
+                    metadata.put("id", product.getId());
+                    metadata.put("name", product.getName());
+                    metadata.put("price", product.getPrice());
 
-                    String content = laptop.getDescription() != null ? laptop.getDescription().toString() : "";
+                    String content = product.getDescription() != null ? product.getDescription().toString() : "";
                     return new Document(content, metadata);
                 })
                 .collect(Collectors.toList());
-
         vectorStore.addDocuments(documents);
     }
 }
